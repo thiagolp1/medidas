82a7233bd64f009b8f3daa8bc63df0a85f1bf910
Thiago <Thiagolopespereiralp1@gmail.com>
2022-03-24 18:34:18 -0300

    finalização do exercicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Double metro, centimetro, km;
+
+Console.Write("Entre com a medida (em metros): ");
+metro = Convert.ToDouble(Console.ReadLine());
+
+centimetro = metro * 100;
+km = metro / 1000;
+
+ Console.WriteLine("\n--- Equivalência ---");
+  Console.WriteLine($"{centimetro} cm");
+  Console.WriteLine($"{metro} m");
+  Console.WriteLine($"{km} Km");
